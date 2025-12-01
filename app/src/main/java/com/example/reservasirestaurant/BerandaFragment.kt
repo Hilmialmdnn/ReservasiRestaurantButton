1dcf9f2f0d0ee3d6be47687e8d7d6e4a997cd508
Lenovo ThinkPad <hilmialmdn@gmail.com>
2025-12-02 00:18:15 +0700

    menambahkan fungsionalitas untuk 3 tombol

--- app/src/main/java/com/example/reservasirestaurant/BerandaFragment.kt
+++ app/src/main/java/com/example/reservasirestaurant/BerandaFragment.kt
@@ -8,10 +8,35 @@ import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(
+    open override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_beranda, container, false)
+    }
+
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnIndoor = view.findViewById<View>(R.id.btnIndoor,)
+       val  btnOudoor = view.findViewById<View>(R.id.btnOutdoor)
+       val btnVvip = view.findViewById<View>(R.id.btnVvip)
+
+       btnIndoor.setOnClickListener {
+           navigateToReservasi()
+       }
+       btnOudoor.setOnClickListener {
+           navigateToReservasi()
+       }
+       btnVvip.setOnClickListener {
+           navigateToReservasi()
+       }
+    }
+
+    private fun navigateToReservasi() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.containerFragment, ReservasiFragment())
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/example/reservasirestaurant/DrinksFragment.kt
+++ app/src/main/java/com/example/reservasirestaurant/DrinksFragment.kt
@@ -1,5 +1,21 @@
 package com.example.reservasirestaurant
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
-class DrinksFragment : Fragment(R.layout.fragment_drinks)
+class DrinksFragment : Fragment(R.layout.fragment_drinks) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnKembaliReservasi = view.findViewById<View>(R.id.btnKembaliReservasi)
+
+        btnKembaliReservasi.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.containerFragment, ReservasiFragment())
+                .addToBackStack(null)
+                .commit()
+
+        }
+    }
+}
